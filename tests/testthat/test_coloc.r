b2fda0964fcd9d38911fa15d96f19a2119c2213f
Tom Palmer <remlapmot@hotmail.com>
2022-01-21 14:38:53 +0000

    Add skips if coloc not installed

--- tests/testthat/test_coloc.r
+++ tests/testthat/test_coloc.r
@@ -9,6 +9,7 @@ vcf1 <- readVcf(fn)
 vcf2 <- readVcf(fn)
 
 test_that("coloc vcf", {
+  skip_if_not_installed("coloc")
 	a <- gwasvcf_to_coloc(vcf1, vcf2, "1:1-100000000")
 	expect_true(is.list(a))
 
@@ -18,6 +19,7 @@ test_that("coloc vcf", {
 
 
 test_that("coloc ieugwasr", {
+  skip_if_not_installed("coloc")
 	chrpos <- "1:109724880-109904880"
 	out <- expect_warning(ieugwasr_to_coloc(id1='ieu-a-300', id2='ieu-a-7', chrompos=chrpos))
 	b <- expect_warning(coloc::coloc.abf(out$dataset1, out$dataset2))
@@ -26,6 +28,7 @@ test_that("coloc ieugwasr", {
 
 
 test_that("arth bbj", {
+  skip_if_not_installed("coloc")
 	chrpos <- "1:38228579-38328579"
 	out <- ieugwasr_to_coloc(id1='bbj-a-73', id2='bbj-a-73', chrompos=chrpos, type1 = "cc", type2 = "cc")
 	res <- coloc::coloc.abf(out$dataset1, out$dataset2)
@@ -34,6 +37,7 @@ test_that("arth bbj", {
 
 
 test_that("coloc ieugwasr 2", {
+  skip_if_not_installed("coloc")
 	chrpos <- "1:47634677-47734677"
 	out <- expect_warning(ieugwasr_to_coloc("ieu-a-2", "eqtl-a-ENSG00000162366", chrpos))
 	res <- expect_warning(coloc::coloc.abf(out$dataset1, out$dataset2))
